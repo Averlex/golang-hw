0e34439c01fc584bbe74cfce431a88161402446d
Averlex <averlex2809@google.com>
2025-02-17 13:27:57 +0300

    Tests added.

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,11 +18,26 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+
+		// Uncomment if task with asterisk completed.
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+
+		// Additional test cases.
+		{input: "\n", expected: "\n"},
+		{input: "\n3", expected: "\n\n\n"},
+		{input: "a", expected: "a"},
+		{input: "a0", expected: ""},
+		{input: `\\`, expected: `\`},
+		{input: `\\a3`, expected: `\aaa`},
+		{input: `\\\\\\`, expected: `\\\`},
+		{input: `\1`, expected: `1`},
+		{input: `\\0`, expected: ``},
+		{input: `\\2\30`, expected: `\\`},
+		{input: `a\1a`, expected: `a1a`},
+		{input: `a0\\0\00`, expected: ``},
 	}
 
 	for _, tc := range tests {
@@ -36,7 +51,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	// Added test cases. Source slice: []string{"3abc", "45", "aaa10b"}.
+	invalidStrings := []string{"3abc", "45", "aaa10b", `\`, `\a`, `\\\`, `\\\a`, `ab\aba`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
