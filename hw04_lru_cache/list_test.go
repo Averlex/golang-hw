06e261b61261d92e0679c166b7e94babdea13bfe
Alexey <averlex2809@gmail.com>
2025-04-16 17:26:20 +0300

    Empty list tests added, single elem list tests implemented

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -43,15 +43,131 @@ func emptyListPushTests(t *testing.T) {
 	}
 }
 
+func emptyListRemoveTest(t *testing.T) {
+	t.Helper()
+	t.Run("remove", func(t *testing.T) {
+		l := NewList()
+		l.Remove(nil)
+
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
+}
+
+func emptyListMoveToFrontTest(t *testing.T) {
+	t.Helper()
+	t.Run("move to front", func(t *testing.T) {
+		testVal := 42
+		l := NewList()
+		elem := &ListItem{Value: testVal}
+		l.MoveToFront(elem)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.Front(), l.Back())
+		require.Equal(t, l.Front(), elem)
+		require.Nil(t, l.Front().Prev)
+		require.Nil(t, l.Back().Next)
+	})
+}
+
 func emptyListTests(t *testing.T) {
 	t.Helper()
 
 	emptyListDataStructureTest(t)
 	emptyListPushTests(t)
+	emptyListRemoveTest(t)
+	emptyListMoveToFrontTest(t)
+}
+
+func singleElemListDataStructureTest(t *testing.T) {
+	t.Helper()
+	t.Run("data structure", func(t *testing.T) {
+		testVal := 42
+		l := NewList()
+		firstElem := l.PushFront(testVal)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.Front(), l.Back())
+		require.Equal(t, l.Front(), firstElem)
+	})
+}
+
+func singleElemListPushTests(t *testing.T) {
+	t.Helper()
+
+	testVal := 42
+
+	testCases := []struct {
+		name                   string
+		len                    int
+		method                 func(List, any) *ListItem
+		headMethod, tailMethod func(List) *ListItem
+	}{
+		{"push front", 2, List.PushFront, List.Front, List.Back},
+		{"push back", 2, List.PushBack, List.Back, List.Front},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			l := NewList()
+			firstElem := tC.method(l, testVal)
+			newElem := tC.method(l, testVal)
+
+			// Data structure testing.
+			require.Equal(t, tC.len, l.Len())
+			require.Equal(t, tC.headMethod(l), newElem)
+			require.Equal(t, tC.tailMethod(l), firstElem)
+
+			// Linkage testing.
+			require.Equal(t, l.Front().Next, l.Back())
+			require.Equal(t, l.Back().Prev, l.Front())
+		})
+	}
+}
+
+func singleElemListRemoveTest(t *testing.T) {
+	t.Helper()
+	t.Run("remove", func(t *testing.T) {
+		testVal := 42
+		l := NewList()
+		elem := l.PushFront(testVal)
+		l.Remove(elem)
+
+		require.Equal(t, 0, l.Len())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+	})
+}
+
+func singleElemListMoveToFrontTest(t *testing.T) {
+	t.Helper()
+	t.Run("move to front", func(t *testing.T) {
+		testVal := 42
+		l := NewList()
+		elem := l.PushFront(testVal)
+		l.MoveToFront(elem)
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.Front(), l.Back())
+		require.Equal(t, l.Front(), elem)
+		require.Nil(t, l.Front().Prev)
+		require.Nil(t, l.Back().Next)
+	})
+}
+
+func singleElemListTests(t *testing.T) {
+	t.Helper()
+
+	singleElemListDataStructureTest(t)
+	singleElemListPushTests(t)
+	singleElemListRemoveTest(t)
+	singleElemListMoveToFrontTest(t)
 }
 
 func TestList(t *testing.T) {
 	t.Run("empty list", func(t *testing.T) { emptyListTests(t) })
+	t.Run("list with a single element", func(t *testing.T) { singleElemListTests(t) })
 
 	// t.Run("complex", func(t *testing.T) {
 	// 	l := NewList()
