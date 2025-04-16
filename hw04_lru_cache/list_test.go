90dd1efcb0d72c7bf6bb12da3d3eec9468486ba2
Alexey <averlex2809@gmail.com>
2025-04-16 16:39:52 +0300

    Empty list tests implemented

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -6,46 +6,84 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestList(t *testing.T) {
-	t.Run("empty list", func(t *testing.T) {
+func emptyListDataStructureTest(t *testing.T) {
+	t.Helper()
+	t.Run("data structure", func(t *testing.T) {
 		l := NewList()
 
 		require.Equal(t, 0, l.Len())
 		require.Nil(t, l.Front())
 		require.Nil(t, l.Back())
 	})
+}
 
-	t.Run("complex", func(t *testing.T) {
-		l := NewList()
+func emptyListPushTests(t *testing.T) {
+	t.Helper()
 
-		l.PushFront(10) // [10]
-		l.PushBack(20)  // [10, 20]
-		l.PushBack(30)  // [10, 20, 30]
-		require.Equal(t, 3, l.Len())
+	testVal := 42
 
-		middle := l.Front().Next // 20
-		l.Remove(middle)         // [10, 30]
-		require.Equal(t, 2, l.Len())
+	testCases := []struct {
+		name   string
+		len    int
+		method func(List, any) *ListItem
+	}{
+		{"push front", 1, List.PushFront},
+		{"push back", 1, List.PushBack},
+	}
 
-		for i, v := range [...]int{40, 50, 60, 70, 80} {
-			if i%2 == 0 {
-				l.PushFront(v)
-			} else {
-				l.PushBack(v)
-			}
-		} // [80, 60, 40, 10, 30, 50, 70]
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			l := NewList()
+			newElem := tC.method(l, testVal)
 
-		require.Equal(t, 7, l.Len())
-		require.Equal(t, 80, l.Front().Value)
-		require.Equal(t, 70, l.Back().Value)
+			require.Equal(t, tC.len, l.Len())
+			require.Equal(t, l.Front(), newElem)
+			require.Equal(t, l.Back(), newElem)
+		})
+	}
+}
 
-		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
-		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
+func emptyListTests(t *testing.T) {
+	t.Helper()
 
-		elems := make([]int, 0, l.Len())
-		for i := l.Front(); i != nil; i = i.Next {
-			elems = append(elems, i.Value.(int))
-		}
-		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
-	})
+	emptyListDataStructureTest(t)
+	emptyListPushTests(t)
+}
+
+func TestList(t *testing.T) {
+	t.Run("empty list", func(t *testing.T) { emptyListTests(t) })
+
+	// t.Run("complex", func(t *testing.T) {
+	// 	l := NewList()
+
+	// 	l.PushFront(10) // [10]
+	// 	l.PushBack(20)  // [10, 20]
+	// 	l.PushBack(30)  // [10, 20, 30]
+	// 	require.Equal(t, 3, l.Len())
+
+	// 	middle := l.Front().Next // 20
+	// 	l.Remove(middle)         // [10, 30]
+	// 	require.Equal(t, 2, l.Len())
+
+	// 	for i, v := range [...]int{40, 50, 60, 70, 80} {
+	// 		if i%2 == 0 {
+	// 			l.PushFront(v)
+	// 		} else {
+	// 			l.PushBack(v)
+	// 		}
+	// 	} // [80, 60, 40, 10, 30, 50, 70]
+
+	// 	require.Equal(t, 7, l.Len())
+	// 	require.Equal(t, 80, l.Front().Value)
+	// 	require.Equal(t, 70, l.Back().Value)
+
+	// 	l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
+	// 	l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
+
+	// 	elems := make([]int, 0, l.Len())
+	// 	for i := l.Front(); i != nil; i = i.Next {
+	// 		elems = append(elems, i.Value.(int))
+	// 	}
+	// 	require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+	// })
 }
