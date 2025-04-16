d694ae728a7f4e7a97b099ab5edeef9dcdbfd026
Alexey <averlex2809@gmail.com>
2025-04-16 18:33:38 +0300

    Behavioral tests implemented

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -301,42 +301,109 @@ func twoElemListTests(t *testing.T) {
 	twoElemListMoveToFrontTest(t)
 }
 
+func complexListTests(t *testing.T) {
+	t.Helper()
+
+	t.Run("behavioral", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+		l.PushBack(30)  // [10, 20, 30]
+		require.Equal(t, 3, l.Len())
+
+		middle := l.Front().Next // 20
+		l.Remove(middle)         // [10, 30]
+		require.Equal(t, 2, l.Len())
+
+		for i, v := range [...]int{40, 50, 60, 70, 80} {
+			if i%2 == 0 {
+				l.PushFront(v)
+			} else {
+				l.PushBack(v)
+			}
+		} // [80, 60, 40, 10, 30, 50, 70]
+
+		require.Equal(t, 7, l.Len())
+		require.Equal(t, 80, l.Front().Value)
+		require.Equal(t, 70, l.Back().Value)
+
+		l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
+		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
+
+		elems := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			elems = append(elems, i.Value.(int))
+		}
+		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+	})
+
+	t.Run("cyclic moving last elem to front", func(t *testing.T) {
+		l := NewList()
+
+		cycleLen := 10
+		expected := make([]int, 0, cycleLen)
+
+		getList := func(l List) []int {
+			elems := make([]int, 0, cycleLen)
+			for i := l.Front(); i != nil; i = i.Next {
+				elems = append(elems, i.Value.(int))
+			}
+			return elems
+		}
+
+		// expected: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+		for i := range cycleLen {
+			l.PushBack(i * 10)
+			expected = append(expected, i*10)
+		}
+
+		require.Equal(t, cycleLen, l.Len())
+		require.Equal(t, expected, getList(l))
+
+		// Clearing
+		for range cycleLen {
+			l.MoveToFront(l.Back())
+		}
+
+		require.Equal(t, expected, getList(l))
+	})
+
+	t.Run("list clearing", func(t *testing.T) {
+		l := NewList()
+
+		cycleLen := 10
+		expected := make([]int, 0, cycleLen)
+
+		getList := func(l List) []int {
+			elems := make([]int, 0, cycleLen)
+			for i := l.Front(); i != nil; i = i.Next {
+				elems = append(elems, i.Value.(int))
+			}
+			return elems
+		}
+
+		// expected: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+		for i := range cycleLen {
+			l.PushBack(i * 10)
+			expected = append(expected, i*10)
+		}
+
+		require.Equal(t, cycleLen, l.Len())
+		require.Equal(t, expected, getList(l))
+
+		// Ciclyc shift
+		for range cycleLen {
+			l.Remove(l.Back())
+		}
+
+		require.Equal(t, 0, l.Len())
+	})
+}
+
 func TestList(t *testing.T) {
 	t.Run("empty list", func(t *testing.T) { emptyListTests(t) })
 	t.Run("list with a single element", func(t *testing.T) { singleElemListTests(t) })
 	t.Run("list with 2 elements", func(t *testing.T) { twoElemListTests(t) })
-
-	// t.Run("complex", func(t *testing.T) {
-	// 	l := NewList()
-
-	// 	l.PushFront(10) // [10]
-	// 	l.PushBack(20)  // [10, 20]
-	// 	l.PushBack(30)  // [10, 20, 30]
-	// 	require.Equal(t, 3, l.Len())
-
-	// 	middle := l.Front().Next // 20
-	// 	l.Remove(middle)         // [10, 30]
-	// 	require.Equal(t, 2, l.Len())
-
-	// 	for i, v := range [...]int{40, 50, 60, 70, 80} {
-	// 		if i%2 == 0 {
-	// 			l.PushFront(v)
-	// 		} else {
-	// 			l.PushBack(v)
-	// 		}
-	// 	} // [80, 60, 40, 10, 30, 50, 70]
-
-	// 	require.Equal(t, 7, l.Len())
-	// 	require.Equal(t, 80, l.Front().Value)
-	// 	require.Equal(t, 70, l.Back().Value)
-
-	// 	l.MoveToFront(l.Front()) // [80, 60, 40, 10, 30, 50, 70]
-	// 	l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
-
-	// 	elems := make([]int, 0, l.Len())
-	// 	for i := l.Front(); i != nil; i = i.Next {
-	// 		elems = append(elems, i.Value.(int))
-	// 	}
-	// 	require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
-	// })
+	t.Run("complex", func(t *testing.T) { complexListTests(t) })
 }
