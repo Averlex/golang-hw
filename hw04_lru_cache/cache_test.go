f8b69da8302aa2b7e2225fcb6c36ef57938bc062
Alexey <averlex2809@gmail.com>
2025-04-18 19:52:03 +0300

    Capacity and single elem cache tests implemented

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestCache(t *testing.T) {
@@ -62,6 +63,9 @@ func TestCache(t *testing.T) {
 		notInCacheChecks(t, &c, "key2")
 		notInCacheChecks(t, &c, "key3")
 	})
+
+	t.Run("incorrect capacity", incorrectCapacity)
+	t.Run("single element cache", cacheSingleItemSuite)
 }
 
 func TestCacheMultithreading(t *testing.T) {
@@ -94,4 +98,103 @@ func notInCacheChecks(t *testing.T, c *Cache, key Key) {
 	v, ok := (*c).Get(key)
 	require.False(t, ok)
 	require.Nil(t, v)
+}
+
+func incorrectCapacity(t *testing.T) {
+	t.Helper()
+
+	t.Run("zero capacity cache is unusable", func(t *testing.T) {
+		c := NewCache(0)
+		require.Nil(t, c)
+	})
+
+	t.Run("negative capacity cache is unusable", func(t *testing.T) {
+		c := NewCache(-1)
+		require.Nil(t, c)
+	})
+}
+
+type CacheTestHelper struct {
+	suite.Suite
+	cache Cache
+}
+
+func (s *CacheTestHelper) isNotInCache(k Key) {
+	v, ok := s.cache.Get(k)
+	s.False(ok)
+	s.Nil(v)
+}
+
+func (s *CacheTestHelper) isInCache(k Key, val interface{}) {
+	v, ok := s.cache.Get(k)
+	s.True(ok)
+	s.Equal(val, v)
+}
+
+func (s *CacheTestHelper) setExisting(k Key, val interface{}) {
+	wasInCache := s.cache.Set(k, val)
+	s.True(wasInCache)
+}
+
+func (s *CacheTestHelper) setNew(k Key, val interface{}) {
+	wasInCache := s.cache.Set(k, val)
+	s.False(wasInCache)
+}
+
+type SingleItemCacheSuite struct {
+	CacheTestHelper
+}
+
+func (s *SingleItemCacheSuite) SetupTest() {
+	s.cache = NewCache(1)
+}
+
+func (s *SingleItemCacheSuite) TestSetToEmpty() {
+	s.setNew("key1", 100)
+}
+
+func (s *SingleItemCacheSuite) TestSetWithUpdate() {
+	s.setNew("key1", 100)
+	s.setExisting("key1", 200)
+}
+
+func (s *SingleItemCacheSuite) TestSetToFull() {
+	s.setNew("key1", 100)
+	s.setNew("key2", 200)
+}
+
+func (s *SingleItemCacheSuite) TestGetFromEmpty() {
+	s.isNotInCache("key1")
+}
+
+func (s *SingleItemCacheSuite) TestGetFromFilled() {
+	s.cache.Set("key1", 100)
+	s.isInCache("key1", 100)
+}
+
+func (s *SingleItemCacheSuite) TestGetEvicted() {
+	s.cache.Set("key1", 100)
+	s.cache.Set("key2", 200)
+	s.isNotInCache("key1")
+}
+
+func (s *SingleItemCacheSuite) TestGetNonExistent() {
+	s.cache.Set("key1", 100)
+	s.isNotInCache("key2")
+}
+
+func (s *SingleItemCacheSuite) TestClearEmpty() {
+	s.cache.Clear()
+	s.isNotInCache("key1")
+}
+
+func (s *SingleItemCacheSuite) TestClearFilled() {
+	s.cache.Set("key1", 100)
+	s.cache.Clear()
+	s.isNotInCache("key1")
+}
+
+func cacheSingleItemSuite(t *testing.T) {
+	t.Helper()
+	suite.Run(t, new(SingleItemCacheSuite))
 }
