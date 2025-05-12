4cdb7aa41ef1008cc1be0de18ba46bea48807970
Averlex <averlex2809@gmail.com>
2025-05-12 23:30:40 +0300

    Applied recommended fixes

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,16 +1,12 @@
 //nolint:revive
 package hw06pipelineexecution
 
-import "context"
-
-//nolint:revive
 type (
 	In  = <-chan interface{}
 	Out = In
 	Bi  = chan interface{}
 )
 
-//nolint:revive
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
@@ -18,94 +14,51 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 		return nil
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-
-	prevStageChan := listenInput(ctx, in)
+	prevStageChan := in
 
 	for _, stage := range stages {
-		prevStageChan = runStage(stage, prevStageChan)
+		prevStageChan = runStage(stage, prevStageChan, done)
 	}
 
-	return sendToOutput(cancel, done, prevStageChan)
+	return prevStageChan
 }
 
-func runStage(stage Stage, in In) Out {
-	// Stage panic protection.
-	return func() (out Out) {
-		// Returning a closed channel on stage running panic.
+func runStage(stage Stage, in In, done In) Out {
+	transit := make(Bi)
+
+	go func() {
+		if done == nil {
+			ch := make(Bi)
+			defer close(ch)
+			done = ch
+		}
+
+		defer close(transit)
+
+		// Draining the channel in case of stop signal.
 		defer func() {
-			if r := recover(); r != nil {
-				tmpChan := make(Bi)
-				close(tmpChan)
-				out = tmpChan
-				awaitChannel(in)
+			for range in {
 			}
 		}()
 
-		out = stage(in)
-		return
-	}()
-}
-
-func sendToOutput(cancel context.CancelFunc, done In, prevStageChan In) Out {
-	res := make(Bi)
-	go func() {
-		// Lazy init of done channel.
-		if done == nil {
-			tmpDone := make(Bi)
-			defer close(tmpDone)
-			done = tmpDone
-		}
-		defer awaitChannel(prevStageChan)
-		defer close(res)
-		defer cancel()
 		for {
 			select {
 			case <-done:
-				return
-			case v, ok := <-prevStageChan:
-				if !ok {
-					return
-				}
-				select {
-				case <-done:
-					return
-				case res <- v:
-					// Passing the value to the next stage.
-				}
-			}
-		}
-	}()
-	return res
-}
-
-func listenInput(ctx context.Context, in In) Out {
-	out := make(Bi)
-	go func() {
-		defer awaitChannel(in)
-		defer close(out)
-		for {
-			select {
-			case <-ctx.Done():
 				return
 			case v, ok := <-in:
 				if !ok {
 					return
 				}
 				select {
-				case <-ctx.Done():
+				case <-done:
 					return
-				case out <- v:
+				case transit <- v:
 				}
 			}
 		}
 	}()
-	return out
-}
 
-func awaitChannel(in In) {
-	go func() {
-		for range in {
-		}
-	}()
+	out := stage(transit)
+
+	return out
 }
