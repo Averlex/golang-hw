5b470fe5f902b80412f2cc9b7513ae96f86bccde
Averlex <averlex2809@gmail.com>
2025-05-22 14:28:46 +0300

    valid test cases added

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require" //nolint:depguard
 )
 
 const defaultDirPath = "./testdata"
@@ -22,16 +22,9 @@ func TestReadDir(t *testing.T) {
 		incorrectFolderContents(t, testDirPath)
 	})
 
-	// file with extension
-	// file with trailing spaces/tabs
-	// file with trailing spaces/tabs in the beginning
-	// file with \x00
-	// empty file
-	// file with multiple lines
-	// testdata case
-
-	// t.Run("valid cases", func(t *testing.T) {
-	// })
+	t.Run("valid cases", func(t *testing.T) {
+		validCases(t, testDirPath)
+	})
 }
 
 func incorrectFolderContents(t *testing.T, testDirPath string) {
@@ -42,7 +35,7 @@ func incorrectFolderContents(t *testing.T, testDirPath string) {
 		path string
 		want error
 	}{
-		{"not a folder", testDirPath + "/some_nonexistent_file.txt", ErrIncorrectPath},
+		{"not a folder", testDirPath + "/not_a_folder.txt", ErrIncorrectPath},
 		{"symlink", testDirPath + "/symlink", ErrIncorrectPath},
 		{"contains folders", testDirPath + "/folders", ErrUnsupportedFile},
 		{"contains devices", testDirPath + "/devices", ErrUnsupportedFile},
@@ -83,6 +76,58 @@ func incorrectFolderContents(t *testing.T, testDirPath string) {
 			env, err := ReadDir(tC.path)
 			require.ErrorIs(t, err, tC.want, "unexpected error received: "+err.Error())
 			require.Nil(t, env, fmt.Sprintf("nil env expected, but it isn't: %T, %v", env, env))
+		})
+	}
+}
+
+func validCases(t *testing.T, testDirPath string) {
+	t.Helper()
+
+	path := testDirPath + "/valid_cases"
+	if err := os.Mkdir(path, os.ModePerm); err != nil {
+		require.Fail(t, "unable to create a folder: "+err.Error())
+	}
+
+	testCases := []struct {
+		name     string
+		fname    string
+		contents string
+		want     string
+	}{
+		{"file with extension", "file_with_extension.txt", "", ""},
+		{"file with trailing spaces/tabs", "file_with_trailing_spaces", "contents\t    ", "contents"},
+		{
+			"file with trailing spaces in the beginning",
+			"file_with_trailing_spaces_in_the_beginning",
+			" \t contents", " \t contents",
+		},
+		{"file with '\\x00'", "file_with_x00", "conte\x00n\x00ts\x00", "conte\nn\nts\n"},
+		{"empty file", "empty_file", "", ""},
+		{"file with multiple lines", "file_with_multiple_lines", "line1\nline2\nline3\n", "line1"},
+	}
+
+	for _, tC := range testCases {
+		if err := os.WriteFile(path+"/"+tC.fname, []byte(tC.contents), os.ModePerm); err != nil {
+			require.Fail(t, "unable to create a file: "+err.Error())
+		}
+	}
+
+	env, err := ReadDir(path)
+	require.NoError(t, err, "unexpected error received")
+	require.NotNil(t, env, fmt.Sprintf("nil env expected, but it isn't: %T, %v", env, env))
+	require.Len(t, env, len(testCases), "unexpected env length")
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			fname := tC.fname
+			if tC.name == "file with extension" {
+				fname = "file_with_extension"
+			}
+			require.Equal(t, tC.want, env[fname].Value, "unexpected value received")
+
+			if tC.name == "empty file" {
+				require.True(t, env[fname].NeedRemove, "unexpected value received")
+			}
 		})
 	}
 }
