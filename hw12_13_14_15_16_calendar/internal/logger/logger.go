8f9f4fea6fc19befdc1d8b5118fa2f4fc6097efe
Averlex <averlex2809@gmail.com>
2025-05-24 23:51:54 +0300

    basic logger implemented

--- hw12_13_14_15_16_calendar/internal/logger/logger.go
+++ hw12_13_14_15_16_calendar/internal/logger/logger.go
@@ -1,20 +1,79 @@
+// Package logger package provides a constructor and wrapper methods
+// for an underlying logger (currently - slog.Logger).
 package logger
 
-import "fmt"
+import (
+	"errors"
+	"log/slog"
+	"os"
+)
 
-type Logger struct { // TODO
+// Logger is a wrapper structure for an underlying logger.
+type Logger struct {
+	l *slog.Logger
 }
 
-func New(level string) *Logger {
-	return &Logger{}
+var (
+	// ErrInvalidLogType  is an error that is returned when the log type is invalid.
+	ErrInvalidLogType = errors.New("invalid log type")
+	// ErrInvalidLogLevel is an error that is returned when the log level is invalid.
+	ErrInvalidLogLevel = errors.New("invalid log level")
+)
+
+// New returns a new Logger with the given log type and level.
+// The log type can be "text" or "json". The log level can be "debug", "info", "warn" or "error".
+// If the log type or level is unknown, it returns an error.
+func New(logType, level string) (*Logger, error) {
+	var logHandler slog.Handler
+	var logLevel slog.Level
+
+	switch level {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	default:
+		return nil, ErrInvalidLogLevel
+	}
+
+	switch logType {
+	case "json":
+		logHandler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: logLevel})
+	case "text":
+		logHandler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: logLevel})
+	default:
+		return nil, ErrInvalidLogType
+	}
+	return &Logger{slog.New(logHandler)}, nil
 }
 
-func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+// Info logs a message with level Info on the standard logger.
+func (logg Logger) Info(msg string, args ...any) {
+	logg.l.Info(msg, args...)
 }
 
-func (l Logger) Error(msg string) {
-	// TODO
+// Error logs a message with level Error on the standard logger.
+func (logg Logger) Error(msg string, args ...any) {
+	logg.l.Error(msg, args...)
+}
+
+// Debug logs a message with level Debug on the standard logger.
+func (logg Logger) Debug(msg string, args ...any) {
+	logg.l.Debug(msg, args...)
+}
+
+// Warn logs a message with level Warn on the standard logger.
+func (logg Logger) Warn(msg string, args ...any) {
+	logg.l.Warn(msg, args...)
+}
+
+// With returns a new Logger that adds the given key-value pairs to the logger's context.
+func (logg Logger) With(args ...any) *Logger {
+	return &Logger{logg.l.With(args...)}
 }
 
 // TODO
