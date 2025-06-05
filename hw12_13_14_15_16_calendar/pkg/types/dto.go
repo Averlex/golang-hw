4ee28b3f8dbc7454a68deb8c207084190ee583f0
Averlex <averlex2809@gmail.com>
2025-06-05 16:14:56 +0300

    dto implemented

---
+++ hw12_13_14_15_16_calendar/pkg/types/dto.go
@@ -0,0 +1,49 @@
+package types
+
+import (
+	"time"
+
+	"github.com/google/uuid" //nolint:depguard,nolintlint
+)
+
+// CreateEventInput represents the input for creating an event.
+//
+//nolint:tagliatelle
+type CreateEventInput struct {
+	Title       string         `json:"title"`
+	Datetime    time.Time      `json:"start_date"`
+	Duration    time.Duration  `json:"end_date"`
+	UserID      string         `json:"user_id"`
+	Description *string        `json:"description,omitempty"`
+	RemindIn    *time.Duration `json:"remind_in,omitempty"`
+}
+
+// UpdateEventInput represents the input for updating an event.
+//
+//nolint:tagliatelle
+type UpdateEventInput struct {
+	ID          uuid.UUID      `json:"id"`
+	Title       *string        `json:"title,omitempty"`
+	Datetime    *time.Time     `json:"start_date,omitempty"`
+	Duration    *time.Duration `json:"end_date,omitempty"`
+	UserID      *string        `json:"user_id,omitempty"`
+	Description *string        `json:"description,omitempty"`
+	RemindIn    *time.Duration `json:"remind_in,omitempty"`
+}
+
+// DateFilterInput represents the input for getters by a fixed period, starting from a specific date.
+//
+//nolint:tagliatelle
+type DateFilterInput struct {
+	Date   time.Time `json:"date"`
+	UserID *string   `json:"user_id"`
+}
+
+// DateRangeInput represents the input for getters by a range of dates.
+//
+//nolint:tagliatelle
+type DateRangeInput struct {
+	DateStart time.Time `json:"date_start"`
+	DateEnd   time.Time `json:"date_end"`
+	UserID    *string   `json:"user_id"`
+}
