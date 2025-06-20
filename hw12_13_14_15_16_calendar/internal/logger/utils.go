e0866527b5f56070e5cb40f0e2ad5d0d55a4af7c
Averlex <averlex2809@gmail.com>
2025-06-20 17:04:01 +0300

    logging of time fields fixed: now format is properly applied to all time fields

--- hw12_13_14_15_16_calendar/internal/logger/utils.go
+++ hw12_13_14_15_16_calendar/internal/logger/utils.go
@@ -10,13 +10,8 @@ import (
 func buildHandler(c *Config) slog.Handler {
 	c.handlerOpts = &slog.HandlerOptions{
 		Level: c.level,
-		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
-			if a.Key == slog.TimeKey {
-				if t, ok := a.Value.Any().(time.Time); ok {
-					a.Value = slog.StringValue(t.Format(c.timeTemplate))
-				}
-			}
-			return a
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			return replaceTimeAttrs(groups, a, c.timeTemplate)
 		},
 	}
 
@@ -28,4 +23,31 @@ func buildHandler(c *Config) slog.Handler {
 	default:
 		return slog.NewJSONHandler(c.writer, c.handlerOpts)
 	}
+}
+
+func replaceTimeAttrs(groups []string, a slog.Attr, timeFormat string) slog.Attr {
+	// Default log tilestamp.
+	if a.Key == slog.TimeKey && len(groups) == 0 {
+		if t, ok := a.Value.Any().(time.Time); ok {
+			a.Value = slog.StringValue(t.Format(timeFormat))
+		}
+		return a
+	}
+
+	// Common time.Time fields.
+	if v, ok := a.Value.Any().(time.Time); ok {
+		a.Value = slog.StringValue(v.Format(timeFormat))
+		return a
+	}
+
+	// Groups handling.
+	if a.Value.Kind() == slog.KindGroup {
+		newGroup := make([]slog.Attr, len(a.Value.Group()))
+		for i, ga := range a.Value.Group() {
+			newGroup[i] = replaceTimeAttrs(append(groups, a.Key), ga, timeFormat)
+		}
+		a.Value = slog.GroupValue(newGroup...)
+	}
+
+	return a
 }
