d39d2281cbd5694d8be5da51e9a2a267a41ce0e8
Averlex <averlex2809@gmail.com>
2025-07-02 19:57:51 +0300

    basic crud tests implemented

--- hw12_13_14_15_16_calendar/internal/server/calendar/grpc/grpc_test.go
+++ hw12_13_14_15_16_calendar/internal/server/calendar/grpc/grpc_test.go
@@ -2,6 +2,7 @@ package grpc_test
 
 import (
 	"context"
+	"errors"
 	"net"
 	"testing"
 	"time"
@@ -85,8 +86,6 @@ func (s *ServerSuite) SetupTest() {
 }
 
 func (s *ServerSuite) TearDownTest() {
-	s.loggerMocks(s.T())
-
 	s.grpcServer.GracefulStop()
 	s.listener.Close()
 	s.app.AssertExpectations(s.T())
@@ -164,6 +163,7 @@ func (s *ServerSuite) TestGetEvent() {
 	for _, tC := range testCases {
 		s.Run(tC.name, func() {
 			tC.mockApp(s.app)
+			s.loggerMocks(s.T())
 			resp, err := s.client.GetEvent(context.Background(), tC.req)
 
 			if tC.expectedCode != codes.OK {
@@ -194,6 +194,492 @@ func (s *ServerSuite) TestGetEvent() {
 				int(resp.Event.Data.RemindIn.AsDuration().Seconds()),
 				"remindIn mismatch",
 			)
+		})
+	}
+}
+
+//nolint:funlen
+func (s *ServerSuite) TestCreateEvent() {
+	input := &types.EventData{
+		Title:       "Test Event",
+		Datetime:    time.Now(),
+		Duration:    time.Hour,
+		Description: "Test Description",
+		UserID:      "user1",
+		RemindIn:    time.Minute * 30,
+	}
+	expectedOutput := &pb.CreateEventResponse{
+		Event: &pb.Event{
+			Id: uuid.New().String(),
+			Data: &pb.EventData{
+				Title:       input.Title,
+				Datetime:    timestamppb.New(input.Datetime),
+				Duration:    durationpb.New(input.Duration),
+				Description: input.Description,
+				UserId:      input.UserID,
+				RemindIn:    durationpb.New(input.RemindIn),
+			},
+		},
+	}
+	reqData := expectedOutput.Event.Data
+
+	prepareData := func() *pb.EventData {
+		data := &pb.EventData{
+			Title:       reqData.Title,
+			Datetime:    reqData.Datetime,
+			Duration:    reqData.Duration,
+			Description: reqData.Description,
+			UserId:      reqData.UserId,
+			RemindIn:    reqData.RemindIn,
+		}
+		return data
+	}
+
+	testCases := []struct {
+		name         string
+		req          *pb.CreateEventRequest
+		mockApp      func(*mocks.Application)
+		want         *pb.CreateEventResponse
+		expectedCode codes.Code
+	}{
+		{
+			name: "success",
+			req:  &pb.CreateEventRequest{Data: reqData},
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(
+					&types.Event{ID: uuid.New(), EventData: *input},
+					nil,
+				).Once()
+			},
+			want:         expectedOutput,
+			expectedCode: codes.OK,
+		},
+		{
+			name: "empty fields/title",
+			req: func() *pb.CreateEventRequest {
+				data := prepareData()
+				data.Title = ""
+				return &pb.CreateEventRequest{Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/duration",
+			req: func() *pb.CreateEventRequest {
+				data := prepareData()
+				data.Duration = nil
+				return &pb.CreateEventRequest{Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/user_id",
+			req: func() *pb.CreateEventRequest {
+				data := prepareData()
+				data.UserId = ""
+				return &pb.CreateEventRequest{Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/datetime",
+			req: func() *pb.CreateEventRequest {
+				data := prepareData()
+				data.Datetime = nil
+				return &pb.CreateEventRequest{Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/description",
+			req: func() *pb.CreateEventRequest {
+				data := prepareData()
+				data.Description = ""
+				return &pb.CreateEventRequest{Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(
+					&types.Event{ID: uuid.New(), EventData: *input}, nil,
+				).Once()
+			},
+			expectedCode: codes.OK,
+			want:         expectedOutput,
+		},
+		{
+			name: "empty fields/remind_in",
+			req: func() *pb.CreateEventRequest {
+				data := prepareData()
+				data.RemindIn = nil
+				return &pb.CreateEventRequest{Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(
+					&types.Event{ID: uuid.New(), EventData: *input}, nil,
+				).Once()
+			},
+			expectedCode: codes.OK,
+			want:         expectedOutput,
+		},
+		{
+			name: "date busy",
+			req:  &pb.CreateEventRequest{Data: reqData},
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(nil, projectErrors.ErrDateBusy).Once()
+			},
+			expectedCode: codes.AlreadyExists,
+		},
+		{
+			name: "internal error",
+			req:  &pb.CreateEventRequest{Data: reqData},
+			mockApp: func(m *mocks.Application) {
+				m.On("CreateEvent", mock.Anything, mock.Anything).Return(nil, errors.New("unexpected error")).Once()
+			},
+			expectedCode: codes.Internal,
+		},
+	}
+
+	for _, tC := range testCases {
+		s.Run(tC.name, func() {
+			tC.mockApp(s.app)
+			s.loggerMocks(s.T())
+			resp, err := s.client.CreateEvent(context.Background(), tC.req)
+
+			if tC.expectedCode != codes.OK {
+				s.Require().Error(err, "expected error, got nil")
+				s.Require().Nil(resp, "expected nil response on error, got non-nil")
+				s.Require().Equal(tC.expectedCode, status.Code(err), "unexpected error code")
+				return
+			}
+
+			s.Require().NoError(err, "unexpected error on GetEvent")
+			s.Require().NotNil(resp, "got nil response, expected non-nil")
+			s.Require().Equal(tC.want.Event.Data.Title, resp.Event.Data.Title, "title mismatch")
+			s.Require().Equal(tC.want.Event.Data.Description, resp.Event.Data.Description, "description mismatch")
+			s.Require().Equal(tC.want.Event.Data.UserId, resp.Event.Data.UserId, "user ID mismatch")
+			s.Require().WithinDuration(
+				tC.want.Event.Data.Datetime.AsTime(),
+				resp.Event.Data.Datetime.AsTime(),
+				time.Second,
+				"datetime mismatch",
+			)
+			s.Require().Equal(
+				int(tC.want.Event.Data.Duration.AsDuration().Seconds()),
+				int(resp.Event.Data.Duration.AsDuration().Seconds()),
+				"duration mismatch",
+			)
+			s.Require().Equal(
+				int(tC.want.Event.Data.RemindIn.AsDuration().Seconds()),
+				int(resp.Event.Data.RemindIn.AsDuration().Seconds()),
+				"remindIn mismatch",
+			)
+		})
+	}
+}
+
+//nolint:funlen
+func (s *ServerSuite) TestUpdateEvent() {
+	id := uuid.New()
+	input := &types.EventData{
+		Title:       "Test Event",
+		Datetime:    time.Now(),
+		Duration:    time.Hour,
+		Description: "Test Description",
+		UserID:      "user1",
+		RemindIn:    time.Minute * 30,
+	}
+	expectedOutput := &pb.UpdateEventResponse{
+		Event: &pb.Event{
+			Id: id.String(),
+			Data: &pb.EventData{
+				Title:       input.Title,
+				Datetime:    timestamppb.New(input.Datetime),
+				Duration:    durationpb.New(input.Duration),
+				Description: input.Description,
+				UserId:      input.UserID,
+				RemindIn:    durationpb.New(input.RemindIn),
+			},
+		},
+	}
+	reqData := expectedOutput.Event.Data
+
+	prepareData := func() *pb.EventData {
+		data := &pb.EventData{
+			Title:       reqData.Title,
+			Datetime:    reqData.Datetime,
+			Duration:    reqData.Duration,
+			Description: reqData.Description,
+			UserId:      reqData.UserId,
+			RemindIn:    reqData.RemindIn,
+		}
+		return data
+	}
+
+	testCases := []struct {
+		name         string
+		req          *pb.UpdateEventRequest
+		mockApp      func(*mocks.Application)
+		want         *pb.UpdateEventResponse
+		expectedCode codes.Code
+	}{
+		{
+			name: "success",
+			req:  &pb.UpdateEventRequest{Id: id.String(), Data: reqData},
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(
+					&types.Event{ID: id, EventData: *input},
+					nil,
+				).Once()
+			},
+			want:         expectedOutput,
+			expectedCode: codes.OK,
+		},
+		{
+			name: "empty fields/id",
+			req: func() *pb.UpdateEventRequest {
+				data := prepareData()
+				return &pb.UpdateEventRequest{Id: "", Data: data}
+			}(),
+			mockApp: func(_ *mocks.Application) {
+				// No calls expected here.
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/title",
+			req: func() *pb.UpdateEventRequest {
+				data := prepareData()
+				data.Title = ""
+				return &pb.UpdateEventRequest{Id: id.String(), Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/duration",
+			req: func() *pb.UpdateEventRequest {
+				data := prepareData()
+				data.Duration = nil
+				return &pb.UpdateEventRequest{Id: id.String(), Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/user_id",
+			req: func() *pb.UpdateEventRequest {
+				data := prepareData()
+				data.UserId = ""
+				return &pb.UpdateEventRequest{Id: id.String(), Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/datetime",
+			req: func() *pb.UpdateEventRequest {
+				data := prepareData()
+				data.Datetime = nil
+				return &pb.UpdateEventRequest{Id: id.String(), Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(
+					nil,
+					projectErrors.ErrInvalidFieldData,
+				).Once()
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "empty fields/description",
+			req: func() *pb.UpdateEventRequest {
+				data := prepareData()
+				data.Description = ""
+				return &pb.UpdateEventRequest{Id: id.String(), Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(
+					&types.Event{ID: id, EventData: *input}, nil,
+				).Once()
+			},
+			expectedCode: codes.OK,
+			want:         expectedOutput,
+		},
+		{
+			name: "empty fields/remind_in",
+			req: func() *pb.UpdateEventRequest {
+				data := prepareData()
+				data.RemindIn = nil
+				return &pb.UpdateEventRequest{Id: id.String(), Data: data}
+			}(),
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(
+					&types.Event{ID: id, EventData: *input}, nil,
+				).Once()
+			},
+			expectedCode: codes.OK,
+			want:         expectedOutput,
+		},
+		{
+			name: "date busy",
+			req:  &pb.UpdateEventRequest{Id: id.String(), Data: reqData},
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(nil, projectErrors.ErrDateBusy).Once()
+			},
+			expectedCode: codes.AlreadyExists,
+		},
+		{
+			name: "permission denied",
+			req:  &pb.UpdateEventRequest{Id: id.String(), Data: reqData},
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(nil, projectErrors.ErrPermissionDenied).Once()
+			},
+			expectedCode: codes.PermissionDenied,
+		},
+		{
+			name: "internal error",
+			req:  &pb.UpdateEventRequest{Id: id.String(), Data: reqData},
+			mockApp: func(m *mocks.Application) {
+				m.On("UpdateEvent", mock.Anything, mock.Anything).Return(nil, errors.New("unexpected error")).Once()
+			},
+			expectedCode: codes.Internal,
+		},
+	}
+
+	for _, tC := range testCases {
+		s.Run(tC.name, func() {
+			tC.mockApp(s.app)
+			s.loggerMocks(s.T())
+			resp, err := s.client.UpdateEvent(context.Background(), tC.req)
+
+			if tC.expectedCode != codes.OK {
+				s.Require().Error(err, "expected error, got nil")
+				s.Require().Nil(resp, "expected nil response on error, got non-nil")
+				s.Require().Equal(tC.expectedCode, status.Code(err), "unexpected error code")
+				return
+			}
+
+			s.Require().NoError(err, "unexpected error on GetEvent")
+			s.Require().NotNil(resp, "got nil response, expected non-nil")
+			s.Require().Equal(tC.want.Event.Data.Title, resp.Event.Data.Title, "title mismatch")
+			s.Require().Equal(tC.want.Event.Data.Description, resp.Event.Data.Description, "description mismatch")
+			s.Require().Equal(tC.want.Event.Data.UserId, resp.Event.Data.UserId, "user ID mismatch")
+			s.Require().WithinDuration(
+				tC.want.Event.Data.Datetime.AsTime(),
+				resp.Event.Data.Datetime.AsTime(),
+				time.Second,
+				"datetime mismatch",
+			)
+			s.Require().Equal(
+				int(tC.want.Event.Data.Duration.AsDuration().Seconds()),
+				int(resp.Event.Data.Duration.AsDuration().Seconds()),
+				"duration mismatch",
+			)
+			s.Require().Equal(
+				int(tC.want.Event.Data.RemindIn.AsDuration().Seconds()),
+				int(resp.Event.Data.RemindIn.AsDuration().Seconds()),
+				"remindIn mismatch",
+			)
+		})
+	}
+}
+
+func (s *ServerSuite) TestDeleteEvent() {
+	id := uuid.New()
+
+	testCases := []struct {
+		name         string
+		req          *pb.DeleteEventRequest
+		mockApp      func(*mocks.Application)
+		expectedCode codes.Code
+	}{
+		{
+			name: "success",
+			req:  &pb.DeleteEventRequest{Id: id.String()},
+			mockApp: func(m *mocks.Application) {
+				m.On("DeleteEvent", mock.Anything, mock.Anything).Return(nil).Once()
+			},
+			expectedCode: codes.OK,
+		},
+		{
+			name: "invalid uuid",
+			req:  &pb.DeleteEventRequest{Id: "invalid-uuid"},
+			mockApp: func(_ *mocks.Application) {
+				// No mock setup as parseUUID will fail before calling Application.
+			},
+			expectedCode: codes.InvalidArgument,
+		},
+		{
+			name: "event not found",
+			req:  &pb.DeleteEventRequest{Id: uuid.New().String()},
+			mockApp: func(m *mocks.Application) {
+				m.On("DeleteEvent", mock.Anything, mock.Anything).Return(projectErrors.ErrEventNotFound).Once()
+			},
+			expectedCode: codes.NotFound,
+		},
+		{
+			name: "internal error",
+			req:  &pb.DeleteEventRequest{Id: uuid.New().String()},
+			mockApp: func(m *mocks.Application) {
+				m.On("DeleteEvent", mock.Anything, mock.Anything).Return(projectErrors.ErrInconsistentState).Once()
+			},
+			expectedCode: codes.Internal,
+		},
+	}
+
+	for _, tC := range testCases {
+		s.Run(tC.name, func() {
+			tC.mockApp(s.app)
+			s.loggerMocks(s.T())
+			resp, err := s.client.DeleteEvent(context.Background(), tC.req)
+
+			if tC.expectedCode != codes.OK {
+				s.Require().Error(err, "expected error, got nil")
+				s.Require().Nil(resp, "expected nil response on error, got non-nil")
+				s.Require().Equal(tC.expectedCode, status.Code(err), "unexpected error code")
+				return
+			}
+
+			s.Require().NoError(err, "unexpected error on DeleteEvent")
+			s.Require().NotNil(resp, "got nil response, expected non-nil")
 		})
 	}
 }
