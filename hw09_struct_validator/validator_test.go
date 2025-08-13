b2c6536111a05dc39f2020238452e1b092bae920
Averlex <averlex2809@gmail.com>
2025-08-13 23:51:30 +0300

    new tests implemented

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -453,3 +453,563 @@ func TestValidate_ValidationErrors(t *testing.T) {
 		})
 	}
 }
+
+func TestValidate_LenCommand(t *testing.T) {
+	type StringField struct {
+		Value string `validate:"len:3"`
+	}
+	type StringSlice struct {
+		Value []string `validate:"len:3"`
+	}
+	type InterfaceSlice struct {
+		Value []any `validate:"len:3"`
+	}
+	type InterfaceField struct {
+		Value any `validate:"len:3"`
+	}
+
+	testCases := []struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			name:        "string/valid",
+			in:          StringField{Value: "asd"},
+			expectedErr: nil,
+		},
+		{
+			name:        "string/lesser len",
+			in:          StringField{Value: "zx"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "string/greater len",
+			in:          StringField{Value: "zxcv"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "interface/valid",
+			in:          InterfaceField{Value: "asd"},
+			expectedErr: nil,
+		},
+		{
+			name:        "interface/lesser len",
+			in:          InterfaceField{Value: "12"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+
+		{
+			name:        "interface/greater len",
+			in:          InterfaceField{Value: "1234"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+
+		{
+			name:        "slice/valid",
+			in:          StringSlice{Value: []string{"asd", "qwe", "bnm", "uop"}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/lesser len",
+			in:          StringSlice{Value: []string{"asd", "qw", "bnm", "up"}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+		{
+			name:        "slice/greater len",
+			in:          StringSlice{Value: []string{"asqd", "qwe", "bnm", "unop"}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+		{
+			name:        "slice/empty slice",
+			in:          StringSlice{Value: []string{}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values",
+			in:          InterfaceSlice{Value: []any{"asg", "ouy"}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values, invalid len",
+			in:          InterfaceSlice{Value: []any{"asgd", "ouy"}},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			t.Parallel()
+			err := Validate(tC.in)
+			var validationErrs ValidationErrors
+			if errors.As(tC.expectedErr, &validationErrs) {
+				var gotErrs ValidationErrors
+				require.ErrorAs(t, err, &gotErrs, "expected ValidationErrors")
+				require.Len(t, gotErrs, len(validationErrs),
+					"unexpected number of validation errors: %d, expected: %d", len(gotErrs), len(validationErrs),
+				)
+				fieldErrCount := make(map[string]int)
+				for _, err := range validationErrs {
+					fieldErrCount[err.Field]++
+				}
+				gotFieldErrCount := make(map[string]int)
+				for _, err := range gotErrs {
+					gotFieldErrCount[err.Field]++
+				}
+				require.Equal(t, fieldErrCount, gotFieldErrCount, "mismatch in validation error counts by field")
+			} else {
+				require.ErrorIs(t, err, tC.expectedErr, "unexpected error type")
+			}
+		})
+	}
+}
+
+func TestValidate_RegexpCommand(t *testing.T) {
+	type StringField struct {
+		Value string `validate:"regexp:^[a-z]+$"`
+	}
+	type StringSlice struct {
+		Value []string `validate:"regexp:^[a-z]+$"`
+	}
+	type InterfaceSlice struct {
+		Value []any `validate:"regexp:^[a-z]+$"`
+	}
+	type InterfaceField struct {
+		Value any `validate:"regexp:^[a-z]+$"`
+	}
+
+	testCases := []struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			name:        "string/valid",
+			in:          StringField{Value: "asd"},
+			expectedErr: nil,
+		},
+		{
+			name:        "string/invalid",
+			in:          StringField{Value: "12345"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "interface/valid",
+			in:          InterfaceField{Value: "asd"},
+			expectedErr: nil,
+		},
+		{
+			name:        "interface/invalid",
+			in:          InterfaceField{Value: "123"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "slice/valid",
+			in:          StringSlice{Value: []string{"asd", "qwe", "bnm", "uop"}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/invalid",
+			in:          StringSlice{Value: []string{"12345", "qw", "0", "up"}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+		{
+			name:        "slice/empty slice",
+			in:          StringSlice{Value: []string{}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values",
+			in:          InterfaceSlice{Value: []any{"asg", "ouy"}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values, invalid regexp",
+			in:          InterfaceSlice{Value: []any{"asgd", "42"}},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			t.Parallel()
+			err := Validate(tC.in)
+			var validationErrs ValidationErrors
+			if errors.As(tC.expectedErr, &validationErrs) {
+				var gotErrs ValidationErrors
+				require.ErrorAs(t, err, &gotErrs, "expected ValidationErrors")
+				require.Len(t, gotErrs, len(validationErrs),
+					"unexpected number of validation errors: %d, expected: %d", len(gotErrs), len(validationErrs),
+				)
+				fieldErrCount := make(map[string]int)
+				for _, err := range validationErrs {
+					fieldErrCount[err.Field]++
+				}
+				gotFieldErrCount := make(map[string]int)
+				for _, err := range gotErrs {
+					gotFieldErrCount[err.Field]++
+				}
+				require.Equal(t, fieldErrCount, gotFieldErrCount, "mismatch in validation error counts by field")
+			} else {
+				require.ErrorIs(t, err, tC.expectedErr, "unexpected error type")
+			}
+		})
+	}
+}
+
+func TestValidate_InCommand(t *testing.T) {
+	type StringField struct {
+		Value string `validate:"in:test1,test2"`
+	}
+	type StringSlice struct {
+		Value []string `validate:"in:test1,test2"`
+	}
+	type InterfaceSlice struct {
+		Value []any `validate:"in:test1,test2"`
+	}
+	type InterfaceField struct {
+		Value any `validate:"in:test1,test2"`
+	}
+	type IntField struct {
+		Value int `validate:"in:10,20"`
+	}
+	type IntSlice struct {
+		Value []int `validate:"in:10,20"`
+	}
+	type IntInterfaceSlice struct {
+		Value []any `validate:"in:10,20"`
+	}
+	type IntInterfaceField struct {
+		Value any `validate:"in:10,20"`
+	}
+
+	testCases := []struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			name:        "string/valid",
+			in:          StringField{Value: "test1"},
+			expectedErr: nil,
+		},
+		{
+			name:        "string/invalid",
+			in:          StringField{Value: "test"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "string interface/valid",
+			in:          InterfaceField{Value: "test2"},
+			expectedErr: nil,
+		},
+		{
+			name:        "string interface/invalid",
+			in:          InterfaceField{Value: "test"},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "int interface/valid",
+			in:          IntInterfaceField{Value: 10},
+			expectedErr: nil,
+		},
+		{
+			name:        "int interface/invalid",
+			in:          IntInterfaceField{Value: 42},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "int/valid",
+			in:          IntField{Value: 10},
+			expectedErr: nil,
+		},
+		{
+			name:        "int/invalid",
+			in:          IntField{Value: 11},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "string slice/valid",
+			in:          StringSlice{Value: []string{"test1", "test1", "test2", "test2"}},
+			expectedErr: nil,
+		},
+		{
+			name:        "string slice/invalid",
+			in:          StringSlice{Value: []string{"test", "test", "test2", "test2"}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+		{
+			name:        "string slice/empty slice",
+			in:          StringSlice{Value: []string{}},
+			expectedErr: nil,
+		},
+		{
+			name:        "string slice/interface values",
+			in:          InterfaceSlice{Value: []any{"test1", "test2"}},
+			expectedErr: nil,
+		},
+		{
+			name:        "string slice/interface values, invalid in",
+			in:          InterfaceSlice{Value: []any{"test", "test2"}},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+
+		{
+			name:        "int slice/valid",
+			in:          IntSlice{Value: []int{20, 10, 20, 20}},
+			expectedErr: nil,
+		},
+		{
+			name:        "int slice/invalid",
+			in:          IntSlice{Value: []int{1, 10, 2, 20}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+		{
+			name:        "int slice/empty slice",
+			in:          IntSlice{Value: []int{}},
+			expectedErr: nil,
+		},
+		{
+			name:        "int slice/interface values",
+			in:          IntInterfaceSlice{Value: []any{20, 10, 10, 20}},
+			expectedErr: nil,
+		},
+		{
+			name:        "int slice/interface values, invalid in",
+			in:          IntInterfaceSlice{Value: []any{1, 10, 20, 20}},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			t.Parallel()
+			err := Validate(tC.in)
+			var validationErrs ValidationErrors
+			if errors.As(tC.expectedErr, &validationErrs) {
+				var gotErrs ValidationErrors
+				require.ErrorAs(t, err, &gotErrs, "expected ValidationErrors")
+				require.Len(t, gotErrs, len(validationErrs),
+					"unexpected number of validation errors: %d, expected: %d", len(gotErrs), len(validationErrs),
+				)
+				fieldErrCount := make(map[string]int)
+				for _, err := range validationErrs {
+					fieldErrCount[err.Field]++
+				}
+				gotFieldErrCount := make(map[string]int)
+				for _, err := range gotErrs {
+					gotFieldErrCount[err.Field]++
+				}
+				require.Equal(t, fieldErrCount, gotFieldErrCount, "mismatch in validation error counts by field")
+			} else {
+				require.ErrorIs(t, err, tC.expectedErr, "unexpected error type")
+			}
+		})
+	}
+}
+
+func TestValidate_MinCommand(t *testing.T) {
+	type IntField struct {
+		Value int `validate:"min:0"`
+	}
+	type IntSlice struct {
+		Value []int `validate:"min:0"`
+	}
+	type InterfaceSlice struct {
+		Value []any `validate:"min:0"`
+	}
+	type InterfaceField struct {
+		Value any `validate:"min:0"`
+	}
+
+	testCases := []struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			name:        "int/valid",
+			in:          IntField{Value: 5},
+			expectedErr: nil,
+		},
+		{
+			name:        "int/valid, border case",
+			in:          IntField{Value: 0},
+			expectedErr: nil,
+		},
+		{
+			name:        "int/invalid",
+			in:          IntField{Value: -42},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "interface/valid",
+			in:          InterfaceField{Value: 5},
+			expectedErr: nil,
+		},
+		{
+			name:        "interface/valid, border case",
+			in:          InterfaceField{Value: 0},
+			expectedErr: nil,
+		},
+		{
+			name:        "interface/invalid",
+			in:          InterfaceField{Value: -42},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "slice/valid",
+			in:          IntSlice{Value: []int{0, 5, 1, 42, 0}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/invalid",
+			in:          IntSlice{Value: []int{0, -1, -42, 42, 0}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+		{
+			name:        "slice/empty slice",
+			in:          IntSlice{Value: []int{}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values",
+			in:          InterfaceSlice{Value: []any{0, 5, 1, 42, 0}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values, invalid min",
+			in:          InterfaceSlice{Value: []any{0, -1, -42, 42, 0}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			t.Parallel()
+			err := Validate(tC.in)
+			var validationErrs ValidationErrors
+			if errors.As(tC.expectedErr, &validationErrs) {
+				var gotErrs ValidationErrors
+				require.ErrorAs(t, err, &gotErrs, "expected ValidationErrors")
+				require.Len(t, gotErrs, len(validationErrs),
+					"unexpected number of validation errors: %d, expected: %d", len(gotErrs), len(validationErrs),
+				)
+				fieldErrCount := make(map[string]int)
+				for _, err := range validationErrs {
+					fieldErrCount[err.Field]++
+				}
+				gotFieldErrCount := make(map[string]int)
+				for _, err := range gotErrs {
+					gotFieldErrCount[err.Field]++
+				}
+				require.Equal(t, fieldErrCount, gotFieldErrCount, "mismatch in validation error counts by field")
+			} else {
+				require.ErrorIs(t, err, tC.expectedErr, "unexpected error type")
+			}
+		})
+	}
+}
+
+func TestValidate_MaxCommand(t *testing.T) {
+	type IntField struct {
+		Value int `validate:"max:100"`
+	}
+	type IntSlice struct {
+		Value []int `validate:"max:100"`
+	}
+	type InterfaceSlice struct {
+		Value []any `validate:"max:100"`
+	}
+	type InterfaceField struct {
+		Value any `validate:"max:100"`
+	}
+
+	testCases := []struct {
+		name        string
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			name:        "int/valid",
+			in:          IntField{Value: 5},
+			expectedErr: nil,
+		},
+		{
+			name:        "int/valid, border case",
+			in:          IntField{Value: 100},
+			expectedErr: nil,
+		},
+		{
+			name:        "int/invalid",
+			in:          IntField{Value: 142},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "interface/valid",
+			in:          InterfaceField{Value: 5},
+			expectedErr: nil,
+		},
+		{
+			name:        "interface/valid, border case",
+			in:          InterfaceField{Value: 100},
+			expectedErr: nil,
+		},
+		{
+			name:        "interface/invalid",
+			in:          InterfaceField{Value: 142},
+			expectedErr: ValidationErrors{{Field: "Value"}},
+		},
+		{
+			name:        "slice/valid",
+			in:          IntSlice{Value: []int{100, 5, 1, 42, 100}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/invalid",
+			in:          IntSlice{Value: []int{100, 100_000, -42, 42, 101}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+		{
+			name:        "slice/empty slice",
+			in:          IntSlice{Value: []int{}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values",
+			in:          InterfaceSlice{Value: []any{0, 5, 1, 42, 0}},
+			expectedErr: nil,
+		},
+		{
+			name:        "slice/interface values, invalid max",
+			in:          InterfaceSlice{Value: []any{101, -1, -42, 42, 142}},
+			expectedErr: ValidationErrors{{Field: "Value"}, {Field: "Value"}},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			t.Parallel()
+			err := Validate(tC.in)
+			var validationErrs ValidationErrors
+			if errors.As(tC.expectedErr, &validationErrs) {
+				var gotErrs ValidationErrors
+				require.ErrorAs(t, err, &gotErrs, "expected ValidationErrors")
+				require.Len(t, gotErrs, len(validationErrs),
+					"unexpected number of validation errors: %d, expected: %d", len(gotErrs), len(validationErrs),
+				)
+				fieldErrCount := make(map[string]int)
+				for _, err := range validationErrs {
+					fieldErrCount[err.Field]++
+				}
+				gotFieldErrCount := make(map[string]int)
+				for _, err := range gotErrs {
+					gotFieldErrCount[err.Field]++
+				}
+				require.Equal(t, fieldErrCount, gotFieldErrCount, "mismatch in validation error counts by field")
+			} else {
+				require.ErrorIs(t, err, tC.expectedErr, "unexpected error type")
+			}
+		})
+	}
+}
